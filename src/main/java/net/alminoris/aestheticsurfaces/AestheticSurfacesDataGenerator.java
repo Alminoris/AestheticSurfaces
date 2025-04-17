212cf2c93efd72affe342a21f069424938a8f892
AOA <aoa@d.co>
2025-04-17 11:00:28 +0300

    Initial commit

---
+++ src/main/java/net/alminoris/aestheticsurfaces/AestheticSurfacesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.alminoris.aestheticsurfaces;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AestheticSurfacesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
