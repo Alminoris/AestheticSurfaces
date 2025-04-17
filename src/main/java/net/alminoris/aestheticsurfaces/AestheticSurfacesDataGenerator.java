ef85ceb01606de9d9198e613f8b0f2e3c354c3ce
AOA <aoa@d.co>
2025-04-17 12:43:03 +0300

    Initial Commit

--- src/main/java/net/alminoris/aestheticsurfaces/AestheticSurfacesDataGenerator.java
+++ src/main/java/net/alminoris/aestheticsurfaces/AestheticSurfacesDataGenerator.java
@@ -1,11 +1,20 @@
 package net.alminoris.aestheticsurfaces;
 
+import net.alminoris.aestheticsurfaces.datagen.ModLootTableProvider;
+import net.alminoris.aestheticsurfaces.datagen.ModModelProvider;
+import net.alminoris.aestheticsurfaces.datagen.ModRecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class AestheticSurfacesDataGenerator implements DataGeneratorEntrypoint {
+public class AestheticSurfacesDataGenerator implements DataGeneratorEntrypoint
+{
 	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator)
+	{
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModRecipeProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
 	}
 }
